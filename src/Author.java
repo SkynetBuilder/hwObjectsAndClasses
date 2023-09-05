1656dc084c84df328d87925721e4cfd8f09b29b3
SkynetBuilder <vlad.gulenkov@gmail.com>
2023-09-05 22:51:21 +0300

    Задание выполнено

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String fistName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+    }
+
+    public String getFistName() {
+        return this.fistName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+}
