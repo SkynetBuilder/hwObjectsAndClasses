437bf1c710601f7510353410123c717f541dad58
SkynetBuilder <vlad.gulenkov@gmail.com>
2023-09-06 18:50:20 +0300

    Исправлена опечатка

--- src/Author.java
+++ src/Author.java
@@ -1,14 +1,14 @@
 public class Author {
-    private String fistName;
+    private String firstName;
     private String lastName;
 
     public Author(String firstName, String lastName) {
-        this.fistName = fistName;
+        this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public String getFistName() {
-        return this.fistName;
+    public String getFirstName() {
+        return this.firstName;
     }
 
     public String getLastName() {
